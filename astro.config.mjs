2f91d9c32def9212e158e49fb8d7c20ddf2ac6c5
mahakal-arch <vedhdawade13@gmail.com>
2026-04-20 19:04:50 +0530

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://xP0Z.github.io', 
+  base: '/ep0z-writeups', 
+});
